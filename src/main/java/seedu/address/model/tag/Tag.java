65d21d8e78d306563cf4d90e01471100399a6749
Li Hao <226736716+Partillay791@users.noreply.github.com>
2025-10-09 11:06:43 +0800

    Revert "Edit tags"

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Tag {
 
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "student|parent";
+    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String tagName;
 
@@ -21,7 +21,6 @@ public class Tag {
      */
     public Tag(String tagName) {
         requireNonNull(tagName);
-        tagName = tagName.toLowerCase();
         checkArgument(isValidTagName(tagName), MESSAGE_CONSTRAINTS);
         this.tagName = tagName;
     }
